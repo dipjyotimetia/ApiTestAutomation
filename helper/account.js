fddf4b5a85ad519bc01b10b34cb631235f105075
Dipjyoti Metia <dipjyotimetia@gmail.com>
2019-04-07 23:23:28 +1000

    added new post api details

---
+++ helper/account.js
@@ -0,0 +1,13 @@
+const account = {
+  request: {
+    baseCurrency: "AUD",
+    accounts: [
+      {
+        accountToken:
+          "obFyWwLGTN2Xk8tZ_Gr40nJXn98Lez2Geppx4MOjLCOTTLBhjLsmWE_aVliknvbv"
+      }
+    ]
+  }
+};
+
+module.exports = account;
--- test/test5.spec.js
+++ test/test5.spec.js
@@ -2,18 +2,31 @@ const should = require("chai").should(),
   expect = require("chai").expect,
   assert = require("chai").assert,
   chalk = require("chalk"),
+  accountPayLoad = require("../helper/account"),
   axios = require("axios");
 
 describe("API Locations", async () => {
-  it("Test Locations", async () => {
-    axios.default
-      .get("https://sandbox.api.nab/v2/accounts?v=1", {
-        headers: {
-          Authorization: "02ebe067-0532-4bd4-937e-1c667214f042"
-        }
-      })
+  axios.defaults.headers = {
+    "Content-Type": "application/json",
+    Authorization: "02ebe067-0532-4bd4-937e-1c667214f042"
+  };
+
+  it("Test Locations details", async () => {
+    axios
+      .get("https://sandbox.api.nab/v2/accounts?v=1")
       .then(res => {
         console.log(res.data.response.accountSummaries);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+
+  it("Test account details", async () => {
+    axios
+      .post("https://sandbox.api.nab/v2/accounts/balance?v=1", accountPayLoad)
+      .then(res => {
+        console.log(res.data.response.accounts);
       })
       .catch(err => {
         console.log(err);
